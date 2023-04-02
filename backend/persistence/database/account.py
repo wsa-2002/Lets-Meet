13a11b0f78b314523b2bf62b7269c5ab8c2dae42
b09705017 <b09705017@ntu.edu.tw>
2023-04-02 11:35:25 +0800

    wsa/disable not verified user login

--- backend/persistence/database/account.py
+++ backend/persistence/database/account.py
@@ -66,7 +66,8 @@ async def read_by_username_or_email(identifier: str, is_google_login: bool = Fal
         sql=fr"SELECT id, pass_hash, is_google_login"
             fr"  FROM account"
             fr" WHERE username = %(username)s"
-            fr"    OR email = %(email)s",
+            fr"    OR email = %(email)s"
+            fr"   AND email IS NOT NULL",
         username=identifier, email=identifier, is_google_login=is_google_login
     )
     try:
