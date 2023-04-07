e68210782e4b17efc937abe8daf635773d7edda9
李昀宸 <b09705015@ntu.edu.tw>
2023-04-07 22:13:32 +0800

    browse meet 功能實作

--- frontend/src/middleware/index.js
+++ frontend/src/middleware/index.js
@@ -3,3 +3,4 @@ import POST from "./simplePost";
 export const { login, signup, forgetPassword, resetPassword, addMeet } = POST;
 export { googleLogin } from "./google";
 export { emailVerification } from "./emailVerification";
+export { browseMeet } from "./meet";
--- frontend/src/middleware/simplePost.js
+++ frontend/src/middleware/simplePost.js
@@ -8,10 +8,15 @@ const URL = {
 };
 
 export default Object.keys(URL).reduce((acc, curr) => {
-  acc[curr] = async (data) => {
+  acc[curr] = async (data, token = undefined) => {
     try {
       console.log("req:", data);
-      const { data: result } = await instance.post(URL[curr], data);
+      console.log();
+      const { data: result } = await instance.post(
+        URL[curr],
+        data,
+        token && { headers: { "auth-token": token } }
+      );
       console.log("res", result);
       return result;
     } catch (error) {
