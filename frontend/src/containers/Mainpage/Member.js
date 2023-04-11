4cf8dbd89d512da7a1e3ec3eb14f9e1dd65203be
b08705058 <b08705058@ntu.edu.tw>
2023-04-11 18:22:57 +0800

    refactor main page

--- frontend/src/containers/Mainpage/Member.js
+++ frontend/src/containers/Mainpage/Member.js
@@ -6,82 +6,82 @@ import { member } from "../../middleware";
 const { Option } = Mentions;
 
 const Member = () => {
-  const [loading, setLoading] = useState(false);
-  const [users, setUsers] = useState([
-    {
-      // key: "No Result",
-      value: "No Result",
-      label: "No Result",
-      disabled: true,
-    },
-  ]);
-  const ref = useRef();
+	const [loading, setLoading] = useState(false);
+	const [users, setUsers] = useState([
+		{
+			// key: "No Result",
+			value: "No Result",
+			label: "No Result",
+			disabled: true,
+		},
+	]);
+	const ref = useRef();
 
-  const loadGithubUsers = async (key) => {
-    if (!key) {
-      setUsers([
-        {
-          // key: "No Result",
-          value: "No Result",
-          label: "No Result",
-          disabled: true,
-        },
-      ]);
-      return;
-    }
-    const {
-      data: { accounts },
-    } = await member(key);
-    console.log(accounts);
-    if (ref.current !== key) return;
-    setLoading(false);
-    setUsers(
-      accounts.length !== 0
-        ? accounts.slice(0, 10).map((m) => ({
-            key: m.username,
-            value: m.username,
-            label: m.username,
-          }))
-        : [
-            {
-              key: "No Result",
-              value: "No Result",
-              label: "No Result",
-              disabled: true,
-            },
-          ]
-    );
-  };
+	const loadGithubUsers = async (key) => {
+		if (!key) {
+			setUsers([
+				{
+					// key: "No Result",
+					value: "No Result",
+					label: "No Result",
+					disabled: true,
+				},
+			]);
+			return;
+		}
+		const {
+			data: { accounts },
+		} = await member(key);
+		console.log(accounts);
+		if (ref.current !== key) return;
+		setLoading(false);
+		setUsers(
+			accounts.length !== 0
+				? accounts.slice(0, 10).map((m) => ({
+						key: m.username,
+						value: m.username,
+						label: m.username,
+				  }))
+				: [
+						{
+							key: "No Result",
+							value: "No Result",
+							label: "No Result",
+							disabled: true,
+						},
+				  ]
+		);
+	};
 
-  const debounceLoadGithubUsers = useCallback(
-    debounce(loadGithubUsers, 800),
-    []
-  );
+	const debounceLoadGithubUsers = useCallback(
+		debounce(loadGithubUsers, 800),
+		[]
+	);
 
-  const onSearch = (search) => {
-    console.log("Search:", search);
-    ref.current = search;
-    setLoading(!!search);
-    setUsers([
-      {
-        // key: "No Result",
-        value: "No Result",
-        label: "No Result",
-        disabled: true,
-      },
-    ]);
-    debounceLoadGithubUsers(search);
-  };
-  return (
-    <Mentions
-      style={{
-        width: "100%",
-      }}
-      loading={loading}
-      onSearch={onSearch}
-      options={users}
-    />
-  );
+	const onSearch = (search) => {
+		console.log("Search:", search);
+		ref.current = search;
+		setLoading(!!search);
+		setUsers([
+			{
+				// key: "No Result",
+				value: "No Result",
+				label: "No Result",
+				disabled: true,
+			},
+		]);
+		debounceLoadGithubUsers(search);
+	};
+	return (
+		<Mentions
+			style={{
+				width: "350px",
+			}}
+			loading={loading}
+			onSearch={onSearch}
+			options={users}
+		/>
+	);
 };
 
 export default Member;
--- frontend/src/containers/Mainpage/index.js
+++ frontend/src/containers/Mainpage/index.js
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 import "@fontsource/roboto/500.css";
-import { Input, Button, DatePicker, TimePicker, Switch, Modal, Form } from "antd";
+import {
+	Input,
+	Button,
+	DatePicker,
+	TimePicker,
+	Switch,
+	Modal,
+	Form,
+} from "antd";
 import { ArrowRightOutlined } from "@ant-design/icons";
 import "../../css/Background.css";
 import { Link, useNavigate } from "react-router-dom";
@@ -15,327 +23,336 @@ const { RangePicker } = DatePicker;
 const { TextArea } = Input;
 
 const JoinMeet = styled.div`
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  width: 310px;
-  left: 50%;
-  top: 180px;
-  transform: translate(-50%, 0);
-  row-gap: 20px;
+	position: relative;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	width: 310px;
+	left: 10%;
+	top: 20%;
+	// transform: translate(-50%, 0);
+	row-gap: 20px;
 `;
 
 const CreateMeet = styled.div`
-  width: 60%;
-  height: 60%;
-  min-width: 300px;
-  min-height: 400px;
-  position: relative;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  // border: 1px solid #D8D8D8;
-  padding: 5% 10%;
+	width: fit-content;
+	min-width: 300px;
+	min-height: 400px;
+	position: relative;
+	left: 10%;
+	top: 20%;
+	// top: 180px;
+	// transform: translate(-50%, -50%);
+	// border: 1px solid #D8D8D8;
+	// padding: 5% 10%;
 `;
 
-const CreateContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  row-gap: 10px;
-`;
+// const CreateContent = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   row-gap: 10px;
+// `;
 
 const Mainpage = () => {
-  const [votingButton, setVotingButton] = useState("hidden");
-  const [meetData, setMeetData] = useState({
-    meet_name: "",
-    start_date: "",
-    end_date: "",
-    start_time_slot_id: 0,
-    end_time_slot_id: 0,
-    gen_meet_url: false,
-    description: "",
-    member_ids: [],
-    emails: [],
-  });
-  const { login, cookies } = useMeet();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const navigate = useNavigate();
-  const [form] = Form.useForm();
+	const [votingButton, setVotingButton] = useState("hidden");
+	const [meetData, setMeetData] = useState({
+		meet_name: "",
+		start_date: "",
+		end_date: "",
+		start_time_slot_id: 0,
+		end_time_slot_id: 0,
+		gen_meet_url: false,
+		description: "",
+		member_ids: [],
+		emails: [],
+	});
+	const { login, cookies } = useMeet();
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const navigate = useNavigate();
+	const [form] = Form.useForm();
 
-  const handleLogin = () => {
-    navigate("/login");
-  };
+	const handleLogin = () => {
+		navigate("/login");
+	};
 
-  const showDate = (e) => {
-    if (votingButton === "hidden") {
-      setVotingButton("visible");
-    } else {
-      setVotingButton("hidden");
-    }
-  };
+	const showDate = (e) => {
+		if (votingButton === "hidden") {
+			setVotingButton("visible");
+		} else {
+			setVotingButton("hidden");
+		}
+	};
 
-  const handleInvite = (e) => {
-    if (e?.key === "Enter" || !e.key) {
-      alert("Invite");
-    }
-  };
+	const handleInvite = (e) => {
+		if (e?.key === "Enter" || !e.key) {
+			alert("Invite");
+		}
+	};
 
-  const handleMeetDataChange =
-    (func, ...name) =>
-    (e) => {
-      if (name.length === 1) {
-        setMeetData((prev) => ({ ...prev, [name[0]]: func(e) }));
-      } else {
-        setMeetData((prev) => ({
-          ...prev,
-          [name[0]]: func(e[0], 1),
-          [name[1]]: func(e[1], 0),
-        }));
-      }
-    };
+	const handleMeetDataChange =
+		(func, ...name) =>
+		(e) => {
+			if (name.length === 1) {
+				setMeetData((prev) => ({ ...prev, [name[0]]: func(e) }));
+			} else {
+				setMeetData((prev) => ({
+					...prev,
+					[name[0]]: func(e[0], 1),
+					[name[1]]: func(e[1], 0),
+				}));
+			}
+		};
 
-  const handleMeetCreate = async () => {
-    try {
-      // console.log(
-      //   moment(
-      //     meetData.voting_end_date + " " + meetData.voting_end_time,
-      //     "YYYY-MM-DD HH-mm-ss"
-      //   ).toISOString()
-      // );
-      if(!login){
-        setIsModalOpen(true);
-      }
-      const result = await AXIOS.addMeet(
-        {
-          ...meetData,
-          voting_end_time: moment(
-            meetData.voting_end_date + " " + meetData.voting_end_time,
-            "YYYY-MM-DD HH-mm-ss"
-          ).toISOString(),
-        },
-        cookies.token
-      );
-    } catch (e) {
-      console.log(e);
-    }
-  };
+	const handleMeetCreate = async () => {
+		try {
+			// console.log(
+			//   moment(
+			//     meetData.voting_end_date + " " + meetData.voting_end_time,
+			//     "YYYY-MM-DD HH-mm-ss"
+			//   ).toISOString()
+			// );
+			if (!login) {
+				setIsModalOpen(true);
+			}
+			const result = await AXIOS.addMeet(
+				{
+					...meetData,
+					voting_end_time: moment(
+						meetData.voting_end_date + " " + meetData.voting_end_time,
+						"YYYY-MM-DD HH-mm-ss"
+					).toISOString(),
+				},
+				cookies.token
+			);
+		} catch (e) {
+			console.log(e);
+		}
+	};
 
-  const handleOk = () => {  // 你這邊再加上ok後要做的動作
-      setIsModalOpen(false);
-  };
-  const handleCancel = () => {
-      setIsModalOpen(false);
-  };
+	const handleOk = () => {
+		// 你這邊再加上ok後要做的動作
+		setIsModalOpen(false);
+	};
+	const handleCancel = () => {
+		setIsModalOpen(false);
+	};
 
-  const CONTENTMENU = {
-    "Event Name*": (
-      <Input
-        style={{ borderRadius: "5px", width: "60%" }}
-        onChange={handleMeetDataChange((i) => i.target.value, "meet_name")}
-      />
-    ),
-    "Voting Period*": (
-      <RangePicker
-        style={{ width: "60%" }}
-        onChange={handleMeetDataChange(
-          (i) => moment(i.toISOString()).format("YYYY-MM-DD"),
-          "start_date",
-          "end_date"
-        )}
-      />
-    ),
-    "Meet Time Period*": (
-      <TimePicker.RangePicker
-        style={{ width: "60%" }}
-        onChange={handleMeetDataChange(
-          (i, plus) => (i.hour() * 60 + i.minute()) / 30 + plus,
-          "start_time_slot_id",
-          "end_time_slot_id"
-        )}
-        minuteStep={30}
-        format={"HH:mm"}
-      />
-    ),
-    Description: (
-      <TextArea
-        style={{
-          height: "120px",
-          width: "60%",
-        }}
-        onChange={handleMeetDataChange((i) => i.target.value, "description")}
-      />
-    ),
-    Member: (
-      <div>
-        <Member style={{ borderRadius: "5px", width: "80%" }} />
-        <Button style={{ background: "#5A8EA4", color: "white" }}>+</Button>
-      </div>
-    ),
-    "Voting Deadline": (
-      <div style={{ columnGap: "10%" }}>
-        <Switch onChange={showDate} />
-        <DatePicker
-          style={{ visibility: votingButton }}
-          onChange={handleMeetDataChange(
-            (i) => moment(i.toISOString()).format("YYYY-MM-DD"),
-            "voting_end_date"
-          )}
-        />
-        <TimePicker
-          style={{ visibility: votingButton }}
-          // name="Voting Deadline Time"
-          onChange={handleMeetDataChange(
-            (i) => moment(i.toISOString()).format("HH-mm-ss"),
-            "voting_end_time"
-          )}
-        />
-      </div>
-    ),
-    "Google Meet URL": (
-      <Switch
-        disabled={!login}
-        onChange={handleMeetDataChange((i) => i, "gen_meet_url")}
-      />
-    ),
-  };
+	const CONTENTMENU = {
+		"Meet Name*": (
+			<Input
+				style={{ borderRadius: "5px", width: "350px" }}
+				onChange={handleMeetDataChange((i) => i.target.value, "meet_name")}
+			/>
+		),
+		"Start/End Date*": (
+			<RangePicker
+				style={{ width: "350px" }}
+				onChange={handleMeetDataChange(
+					(i) => moment(i.toISOString()).format("YYYY-MM-DD"),
+					"start_date",
+					"end_date"
+				)}
+			/>
+		),
+		"Start/End Time*": (
+			<TimePicker.RangePicker
+				style={{ width: "350px" }}
+				onChange={handleMeetDataChange(
+					(i, plus) => (i.hour() * 60 + i.minute()) / 30 + plus,
+					"start_time_slot_id",
+					"end_time_slot_id"
+				)}
+				minuteStep={30}
+				format={"HH:mm"}
+			/>
+		),
+		Member: (
+			<div style={{ display: "flex", flexDirection: "row" }}>
+				<Member style={{ borderRadius: "5px" }} />
+				<Button
+					style={{ background: "#5A8EA4", color: "white", marginLeft: "10px" }}
+				>
+					+
+				</Button>
+			</div>
+		),
+		Description: (
+			<TextArea
+				style={{
+					height: "120px",
+					width: "400px",
+				}}
+				onChange={handleMeetDataChange((i) => i.target.value, "description")}
+			/>
+		),
+		"Voting Deadline": (
+			<div style={{ columnGap: "10%" }}>
+				<Switch onChange={showDate} />
+				<DatePicker
+					style={{ visibility: votingButton }}
+					onChange={handleMeetDataChange(
+						(i) => moment(i.toISOString()).format("YYYY-MM-DD"),
+						"voting_end_date"
+					)}
+				/>
+				<TimePicker
+					style={{ visibility: votingButton }}
+					// name="Voting Deadline Time"
+					onChange={handleMeetDataChange(
+						(i) => moment(i.toISOString()).format("HH-mm-ss"),
+						"voting_end_time"
+					)}
+				/>
+			</div>
+		),
+		"Google Meet URL": (
+			<Switch
+				disabled={!login}
+				onChange={handleMeetDataChange((i) => i, "gen_meet_url")}
+			/>
+		),
+	};
 
-  return (
-    <div className="mainContainer">
-      {login && <Header />}
-      <div className="leftContainer">
-        <JoinMeet>
-          <div
-            style={{
-              fontFamily: "Roboto",
-              fontStyle: "normal",
-              fontWeight: 500,
-              fontSize: "30px",
-              lineHeight: "40px",
-            }}
-          >
-            Join Meet
-          </div>
-          <div
-            style={{ display: "flex", alignItems: "center", columnGap: "10px" }}
-          >
-            <Input
-              placeholder="Invite code"
-              style={{
-                width: "250px",
-                height: "45px",
-                borderRadius: "15px",
-              }}
-              onKeyDown={handleInvite}
-            />
-            <Button
-              type="primary"
-              icon={<ArrowRightOutlined />}
-              size={"large"}
-              style={{
-                background: "#FFD466",
-              }}
-              onClick={handleInvite}
-            />
-          </div>
-        </JoinMeet>
-        <p className="title">Let's Meet!</p>
-      </div>
-      <div className="rightContainer">
-        {!login ? (
-          <Button
-            style={{
-              position: "absolutive",
-              left: "90%",
-              top: "3%",
-              borderRadius: "15px",
-              borderColor: "#FFA601",
-              color: "#FFA601",
-            }}
-            onClick={handleLogin}
-          >
-            Login
-          </Button>
-        ) : (
-          <></>
-        )}
-        <CreateMeet>
-          <div
-            style={{
-              top: 0,
-              left: 0,
-              fontFamily: "Roboto",
-              fontStyle: "normal",
-              fontWeight: "500",
-              fontSize: "30px",
-              lineHeight: "40px",
-              color: "#000000",
-              marginBottom: "30px",
-            }}
-          >
-            Create Meet
-          </div>
-          <div
-            style={{ display: "flex", flexDirection: "column", rowGap: "20px" }}
-          >
-            {Object.keys(CONTENTMENU).map((c, index) => (
-              <div
-                key={index}
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  columnGap: "10%",
-                }}
-              >
-                <div style={{ width: "200px" }}>{c}</div>
-                {CONTENTMENU[c]}
-              </div>
-            ))}
-          </div>
-          <CreateContent></CreateContent>
-          <Button
-            style={{
-              position: "absolutive",
-              left: "50%",
-              top: "30px",
-              borderRadius: "15px",
-              background: "#B3DEE5",
-              transform: "translate(-50%, 0)",
-            }}
-            size="large"
-            onClick={handleMeetCreate}
-          >
-            Create
-          </Button>
-        </CreateMeet>
-        <Modal title="" style={{fontFamily: "Nunito"}}
-            open={isModalOpen} onOk={handleOk} onCancel={handleCancel} okText="Ok"
-            cancelText="Cancel">
-            <Form form={form} layout="vertical" name="form_in_modal">
-                <Form.Item
-                    name="name"
-                    label="Please enter your name"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Error: Please enter your name!',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-            </Form>
-        </Modal>
-      </div>
-      <div className="leftFooter">
-        <div>中文 | English</div>
-      </div>
-      <div className="rightFooter">
-        <div>Copyright 2023</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="mainContainer">
+			{login && <Header />}
+			<div className="leftContainer">
+				<JoinMeet>
+					<div
+						style={{
+							fontFamily: "Roboto",
+							fontStyle: "normal",
+							fontWeight: 500,
+							fontSize: "30px",
+							lineHeight: "40px",
+						}}
+					>
+						Join Meet
+					</div>
+					<div
+						style={{ display: "flex", alignItems: "center", columnGap: "10px" }}
+					>
+						<Input
+							placeholder="Invitation code"
+							style={{
+								width: "250px",
+								height: "45px",
+								borderRadius: "15px",
+							}}
+							onKeyDown={handleInvite}
+						/>
+						<Button
+							type="primary"
+							icon={<ArrowRightOutlined />}
+							size={"large"}
+							style={{
+								background: "#FFD466",
+							}}
+							onClick={handleInvite}
+						/>
+					</div>
+				</JoinMeet>
+				<p className="title">Let's Meet!</p>
+			</div>
+			<div className="rightContainer">
+				{!login && (
+					<Button
+						style={{
+							position: "absolute",
+							left: "90%",
+							top: "3%",
+							borderRadius: "15px",
+							borderColor: "#FFA601",
+							color: "#FFA601",
+						}}
+						onClick={handleLogin}
+					>
+						Login
+					</Button>
+				)}
+				<CreateMeet>
+					<div
+						style={{
+							// top: 0,
+							// left: 0,
+							fontFamily: "Roboto",
+							fontStyle: "normal",
+							fontWeight: "500",
+							fontSize: "30px",
+							lineHeight: "40px",
+							color: "#000000",
+							marginBottom: "30px",
+						}}
+					>
+						Create Meet
+					</div>
+					<div
+						style={{ display: "flex", flexDirection: "column", rowGap: "20px" }}
+					>
+						{Object.keys(CONTENTMENU).map((c, index) => (
+							<div
+								key={index}
+								style={{
+									display: "flex",
+									alignItems: "center",
+									// columnGap: "10%",
+									justifyContent: "flex-start",
+								}}
+							>
+								<div style={{ width: "200px" }}>{c}</div>
+								{CONTENTMENU[c]}
+							</div>
+						))}
+						<div style={{ display: "flex", justifyContent: "flex-end" }}>
+							<Button
+								style={{
+									top: "30px",
+									borderRadius: "15px",
+									background: "#B3DEE5",
+								}}
+								size="large"
+								onClick={handleMeetCreate}
+							>
+								Create
+							</Button>
+						</div>
+					</div>
+				</CreateMeet>
+				<Modal
+					title=""
+					style={{ fontFamily: "Nunito" }}
+					open={isModalOpen}
+					onOk={handleOk}
+					onCancel={handleCancel}
+					okText="Ok"
+					cancelText="Cancel"
+				>
+					<Form form={form} layout="vertical" name="form_in_modal">
+						<Form.Item
+							name="name"
+							label="Please enter your name"
+							rules={[
+								{
+									required: true,
+									message: "Error: Please enter your name!",
+								},
+							]}
+						>
+							<Input />
+						</Form.Item>
+					</Form>
+				</Modal>
+			</div>
+			<div className="middleContainer"></div>
+			<div className="leftFooter">
+				<div>中文 | English</div>
+			</div>
+			<div className="rightFooter">
+				<div>Copyright 2023</div>
+			</div>
+		</div>
+	);
 };
 
 export default Mainpage;
