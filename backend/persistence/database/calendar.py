5a3d6958d28e99fe13f4354bbd96a7638b527c0f
b09705004 <b09705004@ntu.edu.tw>
2023-04-26 13:33:38 +0800

    fixed data bug

--- backend/persistence/database/calendar.py
+++ backend/persistence/database/calendar.py
@@ -5,8 +5,8 @@ from base import vo
 
 async def get_event(account_id: str, start_date: date, end_date: date):
     sql, params = pyformat2psql(
-        sql=fr"  SELECT invite_code, meet.finalized_start_date, meet.finalized_end_date,"
-            fr"  title, finalized_start_time_slot_id, finalized_end_time_slot_id" 
+        sql=fr"  SELECT invite_code, title, meet.finalized_start_date, meet.finalized_end_date,"
+            fr"  finalized_start_time_slot_id, finalized_end_time_slot_id" 
             fr"  FROM event"
             fr"  INNER JOIN meet"  
             fr"  ON event.meet_id = meet.id"
