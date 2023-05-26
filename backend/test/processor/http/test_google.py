21bdeb7f761649a5f5140484a277e5e24833b621
Lichi Lin <b09705003@ntu.edu.tw>
2023-05-27 00:36:22 +0800

    fake data

--- backend/test/processor/http/test_google.py
+++ backend/test/processor/http/test_google.py
@@ -9,13 +9,13 @@ class TestGoogleLogin(unittest.IsolatedAsyncioTestCase):
     def setUp(self) -> None:
         self.token = {
             'userinfo':{
-                'email': 'lichi10404@gmail.com'
+                'email': 'lichi@gmail.com'
             },
-            'access_token': 'ya29.a0AWY7CkmmbQ9YiCSNnLZMNiIds3_IYScRVcBAVJVn-r0XVYgquEMY3xguh4fWgOeAJxROaq5ywS2PyYl9frCwl-PZHkYQCt_Wnzvw06MMpalKIjeYbKxo9mO3JD_-aaSY56JXpJjNETxhfJowC-jRzPlbHUZXaCgYKAVQSARISFQG1tDrpYBC3qShPTglq5FaTQQHNYQ0163',
-            'refresh_token':'1//0er3qCk8MbD-9CgYIARAAGA4SNgF-L9IrFddYHlBhmTeH3d3WNKcvB-iMAR5H3L2nh5a9gWnt5o8FnTmMfeE73cO4QvzE0ighPA'
+            'access_token': 'access_token',
+            'refresh_token':'refresh_token'
         }
-        query_string = 'state=sRCWEguBv3Pj19yEqzMMt9kg7UwjJ0&code=4%2F0AbUR2VPuytINU3Hd5Ey1hZm8xRCyhstkw9bQ8e0ZO6_jAj3dlmNwxIGFeAk8hf1RPYqwrQ&scope=email+profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar+openid+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&authuser=1&prompt=consent'
-        query_string_denided = 'error=access_denied&state=bqA3HXNmHrk8wrkbiR0InAqLdjINGL'
+        query_string = 'query_string'
+        query_string_denided = 'error=access_denied'
         
         self.input_data = Request(scope={'type': 'http', 'path': '/', 'query_string': query_string.encode('utf-8')})
         
